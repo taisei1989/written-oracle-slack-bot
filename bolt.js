060d3f2f19d4e0f0d5cfb0be624d9b61068e1138
taisei yamashita <taisei6514@gmail.com>
2022-05-21 16:59:46 +0900

    おみくじ機能を追加

--- bolt.js
+++ bolt.js
@@ -14,4 +14,10 @@ app.message(/hello/i, ({ message, say }) => {
   say(`こんにちは！ <@${message.user}>さん`);
 });
 
+app.message(/おみくじ/, ({ message, say }) => {
+  const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  say(`<@${message.user}>さんの今日の運勢は${lot}です！`);
+});
+
 app.start();
